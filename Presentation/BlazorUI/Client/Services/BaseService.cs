8377e49728553275fdd6378760622e850ca17a65
Emrah Atalay <emrahatalay92@gmail.com>
2020-06-06 23:43:28 +0300

    product grid sayfası eklendi.

--- Presentation/BlazorUI/Client/Services/BaseService.cs
+++ Presentation/BlazorUI/Client/Services/BaseService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BlazorUI.Client.Services
